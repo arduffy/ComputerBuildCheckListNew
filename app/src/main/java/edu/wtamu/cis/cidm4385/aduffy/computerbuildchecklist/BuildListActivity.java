f38870a365f9980699500c7672683df9821bb7e5
Amanda <arduffy>
2018-02-23 11:15:32 -0600

    initial branch commit

--- app/src/main/java/edu/wtamu/cis/cidm4385/aduffy/computerbuildchecklist/BuildListActivity.java
+++ app/src/main/java/edu/wtamu/cis/cidm4385/aduffy/computerbuildchecklist/BuildListActivity.java
@@ -2,10 +2,10 @@ package edu.wtamu.cis.cidm4385.aduffy.computerbuildchecklist;
 
 import android.support.v4.app.Fragment;
 
-public class ListActivity extends SingleFragmentActivity {
+public class BuildListActivity extends SingleFragmentActivity {
 
     @Override
     protected Fragment createFragment() {
-        return new CrimeFragment();
+        return new CrimeListFragment();
     }
 }
