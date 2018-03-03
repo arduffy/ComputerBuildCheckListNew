953852c8f7609e0a560c742eedbc2daedcdb02ca
Amanda Duffy <arduffy26@yahoo.com>
2018-03-03 13:27:50 -0600

    worked on the Fragment

--- app/src/main/java/wt/cidm4385/computerbuildchecklistnew/CheckListActivityFragment.java
+++ app/src/main/java/wt/cidm4385/computerbuildchecklistnew/CheckListActivityFragment.java
@@ -1,12 +1,16 @@
 package wt.cidm4385.computerbuildchecklistnew;
 
 import android.app.Fragment;
+import android.app.ListFragment;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -16,39 +20,29 @@ import java.util.List;
  * Created by ad939564 on 2/28/2018.
  */
 
-public class CheckListActivityFragment extends Fragment {
-
-    ArrayAdapter<String> mTaskAdapter;
-
-    public CheckListActivityFragment() {
-    }
-
+public class CheckListActivityFragment extends ListFragment implements AdapterView.OnItemClickListener {
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_check_list, container, false);
-
-        //Create the fake data
-        String[] fakeData = {
-                "Revise for exam",
-                "Buy milk",
-                "Do laundry",
-                "Call Melissa",
-                "Buy stamps",
-        };
-        List<String> tasks = new ArrayList<String>(Arrays.asList(fakeData));
-
-        //Create the ArrayAdapter by specifying context, "how" (layout),"where" (textview), and " what" (data)
-        mTaskAdapter = new ArrayAdapter<String>(
-                getActivity(),
-                R.layout.list_item,
-                R.id.list_item_task_textview,
-                tasks);
-
-        //Still need to bind adapter to the ListView
-        ListView listView = (ListView) view.findViewById(R.id.list_item_task_textview);
-        listView.setAdapter(mTaskAdapter);
+        public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_check_list, container,false);
 
         return view;
     }
+
+    @Override
+    public void onActivityCreated (@Nullable Bundle savedInstanceState){
+            super.onActivityCreated(savedInstanceState);
+
+            ArrayAdapter arrayAdapter = ArrayAdapter.createFromResource(getActivity(),R.array.build_list,android.R.layout.simple_list_item_1);
+            setListAdapter(arrayAdapter);
+            getListView().setOnItemClickListener(this);
+    }
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id){
+        String[] buildList = getActivity().getResources().getStringArray(R.array.build_list);
+        Toast toast = Toast.makeText(getActivity(), "Done" + buildList[position], Toast.LENGTH_SHORT.show());
+    }
 }
+
+
+
