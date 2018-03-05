e12c5b6ad0a66269e9cbd4d8dbd30dddc3d3ce07
Amanda <arduffy>
2018-03-05 08:57:06 -0600

    created array adapter and onclicklistener

---
+++ app/src/main/java/edu/wtamu/cis/cidm4385/aduffy/computerbuildchecklist/CheckListFragment.java
@@ -0,0 +1,44 @@
+package edu.wtamu.cis.cidm4385.aduffy.computerbuildchecklist;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+/**
+ * Created by Amanda on 3/5/2018.
+ */
+
+public class CheckListFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View view = inflater.inflate(R.layout.fragment_list, container, false);
+
+        return view;
+    }
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState)  {
+        super.onActivityCreated(savedInstanceState);
+
+        ArrayAdapter arrayAdapter = ArrayAdapter.createFromResource(getActivity(),R.array.build_list,android.R.layout.simple_list_item_1);
+        setListAdapter(arrayAdapter);
+        getListView().setOnItemClickListener(this);
+    }
+
+
+    @Override
+    public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+        String[] buildList = getActivity().getResources().getStringArray(R.array.build_list);
+
+        Toast.makeText(getActivity(), "Done! " + buildList[position], Toast.LENGTH_SHORT).show();
+    }
+}
