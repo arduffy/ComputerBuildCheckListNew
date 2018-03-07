a828af2c8a3be5cc60298cc503844071e3f48119
Bradone911 <baholland1@buffs.wtamu.edu>
2018-03-07 09:33:52 -0600

    multi boxs but are not aligned right.

--- app/src/main/java/edu/wtamu/cis/cidm4385/aduffy/computerbuildchecklist/CheckListFragment.java
+++ app/src/main/java/edu/wtamu/cis/cidm4385/aduffy/computerbuildchecklist/CheckListFragment.java
@@ -9,9 +9,7 @@ import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Toast;
-/**
- * Created by Amanda on 3/5/2018.
- */
+
 
 public class CheckListFragment extends ListFragment implements AdapterView.OnItemClickListener {
 
@@ -40,4 +38,5 @@ public class CheckListFragment extends ListFragment implements AdapterView.OnIte
 
         Toast.makeText(getActivity(), "Add " + buildList[position], Toast.LENGTH_SHORT).show();
     }
+
 }
--- app/src/main/java/edu/wtamu/cis/cidm4385/aduffy/computerbuildchecklist/ListActivity.java
+++ app/src/main/java/edu/wtamu/cis/cidm4385/aduffy/computerbuildchecklist/ListActivity.java
@@ -2,12 +2,122 @@ package edu.wtamu.cis.cidm4385.aduffy.computerbuildchecklist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class ListActivity extends AppCompatActivity {
+//    private ArrayList selection;
+
+    ArrayList<String> selection = new ArrayList<String>();
+//    TextView final_text;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_list);
+
+//        final_text = (TextView) findViewById(R.id.final_result);
+//        final_text.setEnabled(false);
+
+    }
+
+    public void selectItem(View view) {
+
+        boolean checked = ((CheckBox) view).isChecked();
+        switch (view.getId()) {
+            case R.id.part_tower:
+
+                if (checked) {
+                    selection.add("ATX Tower Case");
+                } else {
+                    selection.remove("ATX Tower Case");
+                }
+                break;
+
+            case R.id.part_power:
+
+                if (checked) {
+                    selection.add("Power Supply");
+                } else {
+                    selection.remove("Power Supply");
+                }
+                break;
+
+            case R.id.part_cpu:
+
+                if (checked) {
+                    selection.add("CPU");
+                } else {
+                    selection.remove("CPU");
+                }
+                break;
+
+            case R.id.part_board:
+
+                if (checked) {
+                    selection.add("Mother Board");
+                } else {
+                    selection.remove("Mother Board");
+                }
+                break;
+
+            case R.id.part_processor:
+
+                if (checked) {
+                    selection.add("Processor");
+                } else {
+                    selection.remove("Processor");
+                }
+                break;
+
+            case R.id.part_memory:
+
+                if (checked) {
+                    selection.add("RAM Memory");
+                } else {
+                    selection.remove("RAM Memory");
+                }
+                break;
+
+            case R.id.part_hard_drive:
+
+                if (checked) {
+                    selection.add("Hard Drive");
+                } else {
+                    selection.remove("Hard Drive");
+                }
+                break;
+
+            case R.id.part_video_card:
+
+                if (checked) {
+                    selection.add("Graphics/Video Card");
+                } else {
+                    selection.remove("Graphics/Video Card");
+                }
+                break;
+
+            case R.id.part_sound_card:
+
+                if (checked) {
+                    selection.add("Sound Card");
+                } else {
+                    selection.remove("Sound Card");
+                }
+                break;
+
+            case R.id.part_fan:
+
+                if (checked) {
+                    selection.add("Cooling Fan");
+                } else {
+                    selection.remove("Cooling Fan");
+                }
+                break;
+        }
+
     }
 }
